afd059195595554a974114109bb27c5b11a56ce0
Kieran Caraway <kierancaraway20@yahoo.com>
2024-09-19 21:41:42 -0500

    latest update
    edited JavaScript functionality. instead of creating new elements per click now the clicks updates existing elements.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,43 @@
 let gameBoardButtons = document.querySelectorAll(".gameButtonz")
-
-
 let locationStart = Math.floor(Math.random() * gameBoardButtons.length);
+let locationStart2 = Math.floor(Math.random() * gameBoardButtons.length);
+let locationStart3 = Math.floor(Math.random() * gameBoardButtons.length);
+
+let battleship = document.getElementById("battleship");
+let hitResultsIMG = document.querySelector(".hitOrMiss");
+let hitText = document.querySelector(".hitOrMissText");
+
 
 
 for (let i = 0; i < gameBoardButtons.length; i++) {
-    if (i === locationStart) {
+    if (i === locationStart || i === locationStart2 || i === locationStart3) {
         gameBoardButtons[i].addEventListener("click", () => {
             gameBoardButtons[i].style.backgroundColor = "green"
-        }) 
+        
+            hitResultsIMG.src = "media/kaboom-explosive.gif";
+            hitText.innerHTML = "GREAT JOB! YOU LANDED A HIT!";
+            
+            //* change content within new div and div items
+        })
     }
     else {
-            gameBoardButtons[i].addEventListener("click", () => {
-                gameBoardButtons[i].style.backgroundColor = "red"
-            })
-        }
-}
+        gameBoardButtons[i].addEventListener("click", () => {
+            gameBoardButtons[i].style.backgroundColor = "red"
+    
+            hitResultsIMG.src = "media/dodge-bullets.gif";
+            hitText.innerHTML = "YOU MISSED! TRY AGAIN!";
+        })
+    }
+};
+
+
+
+//todo if the button turns red, new text should appear that says you missed. Vice versa for Green
+
+//todo I could also add an image every time i missed/hit.
+
+
+
 
 
 
